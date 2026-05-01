0a3077864be813270a0c53bfaf3b69ce14e031a7
TanyaVaish-17 <tanyavaish05@gmail.com>
2026-05-02 01:39:20 +0530

    level order traversal

---
+++ trees/05_levelOrder.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+vector<vector<int>> levelOrder(Node* root){
+    vector<vector<int>> ans;
+    if (root==NULL) return ans;
+    queue<Node*> q;
+    q.push(root);
+    while (!q.empty()){
+        int size=q.size();
+        vector<int> level;
+        for (int i=0;i<size;i++){
+            Node* node=q.front();
+            q.pop();
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+int main(){
+    struct Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    vector<vector<int>> ans = levelOrder(root);
+    for (auto &level :ans){
+        for (int x : level){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
